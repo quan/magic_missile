db98f7deb662871f268177a63fe45be6695bf021
Nick Isber <nick.isber@gmail.com>
2016-01-14 21:12:39 -0800

    Changed the Progress Bar

---
+++ character/suex/default.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<div style="width: 100%; height: 100%;">
+	<div class="title">Please Choose Your Suex.</div>
+
+	<p>
+		These are words that help you pick your suex stuff or whatever.
+	</p>
+
+
+</div>
