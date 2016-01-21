cfbdeb1e3efb3d88cf1d782dfb99ec5c9fc0eadb
Nick Isber <nick.isber@gmail.com>
2016-01-21 13:55:11 -0800

    Removed unnecessary header html

--- character/rolls/default.php
+++ character/rolls/default.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<!--JS-->
-	<script type="text/javascript" src="../js/jquery-1.11.3.js"></script>
-	<script type="text/javascript" src="../js/main.js"></script>
-	<!--CSS-->
-	<link rel="stylesheet" type="text/css" href="rolls.css" />
-</head>
+<div class="title">
+	Please Select a Method
+</div>
 
-<body>
-	
-	<div class="title">
-		Please Select a Method
-	</div>
-
-	<p class="welcome">
-		These are words about the different methods for obtaining your ability scores.
-	</p>
-
-
-</body>
-</html>
+<p class="welcome">
+	These are words about the different methods for obtaining your ability scores.
+</p>
